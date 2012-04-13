7b611046991e2469d85b5679a5f1b6156c6f1057
Joji Antony <simula67@gmail.com>
2012-04-13 11:30:09 -0400

    Compile success

---
+++ struct.h
@@ -0,0 +1,28 @@
+#ifndef _STRUCT_H_
+#define _STRUCT_H_
+
+struct data {
+  double rpm;
+  double compratio;
+  double ncycl;
+  double gamma;
+  double B;
+  double L;
+  double R;
+  double Ui;
+  double Bv;
+  double Liv;
+  double Ap;
+  double Av;
+  double atmopres;
+  double atmotemp;
+  double Tw;
+  double sacomb;
+  double facomb;
+  double Vc;
+  double omega;
+
+
+};
+
+#endif
